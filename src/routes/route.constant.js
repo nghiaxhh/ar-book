a52d423f222649f0094dbaaaf2a1f16b0dd9f685
nghiahh <hieunghia1909@gmail.com>
2023-08-22 10:32:02 +0700

    update model

--- src/routes/route.constant.js
+++ src/routes/route.constant.js
@@ -2,5 +2,9 @@ export const ROUTE_PATH = {
   HOME: '/',
   CHARACTER: '/character',
   PREVIEW_MODEL: '/preview',
-  INTRODUCE: '/introduce'
+  INTRODUCE: '/introduce',
+  MODEL_BRAIN: '/model-brain',
+  MODEL_STOMACH: '/model-stomach',
+  MODEL_INTESTINE: '/model-intestine',
+  MODEL_CHARACTER: '/model-character'
 };
