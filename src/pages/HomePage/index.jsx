6cc09792a316beaab62f8e7d4a677fcea5601d45
hohnghia <hieunghia1909@gmail.com>
2023-11-14 15:31:17 +0700

    update home page, introduce page

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -9,30 +9,50 @@ const HomePage = () => {
 
   return (
     <BaseLayout>
-      <HeaderWrapper
-        style={{
-          backgroundImage: `url(${process.env.PUBLIC_URL}/images/HomeImg.png)`
-        }}
-      >
+      <HeaderWrapper>
         <div
-          style={{
-            display: 'flex',
-            justifyContent: 'center',
-            paddingTop: '20px'
-          }}
+          style={{ display: 'flex', paddingTop: '50px', alignItems: 'center' }}
         >
+          <img
+            src={process.env.PUBLIC_URL + `/images/character.png`}
+            width={'180px'}
+            alt=''
+          />
+          <div className='box-container'>
+            <img
+              src={process.env.PUBLIC_URL + `/images/organ.png`}
+              width={'280px'}
+              alt=''
+            />
+            <div className='main-title'>たけしくんの ふしぎな たび</div>
+            <div className='sub-title'>
+              ～たべものを たべたら どうなるの？～
+            </div>
+          </div>
+          <img
+            src={process.env.PUBLIC_URL + `/images/genki.png`}
+            width={'150px'}
+            alt=''
+          />
+        </div>
+        <div
+          onClick={() => navigate(ROUTE_PATH.INTRODUCE)}
+          className='btn-action'
+        >
+          えほんを よむ
+        </div>
+
+        <div className='home-page-footer'>
           <img
             src={process.env.PUBLIC_URL + '/images/logo.png'}
             className='imgLogo'
             alt=''
           />
-        </div>
-        <div
-          om
-          onClick={() => navigate(ROUTE_PATH.INTRODUCE)}
-          className='btn-action'
-        >
-          読みはじめる
+          <img
+            src={process.env.PUBLIC_URL + '/images/organ_project.png'}
+            alt=''
+            className='imgLogo'
+          />
         </div>
       </HeaderWrapper>
     </BaseLayout>
